f909e2c5717b75616d4865d2677b2cb827e16348
Colin Shanny <cshannyy@gmail.com>
2020-11-23 17:40:32 -0600

    re-done

---
+++ SQLExercise1.sql
@@ -0,0 +1,58 @@
+-- find all products--
+select * from products;
+ 
+-- find all products that cost $1400
+select * from products
+where price = 1400;
+ 
+-- find all products that cost $11.99 or $13.99
+select * from products
+where price = 11.99 or 13.99;
+ 
+-- find all products that do NOT cost 11.99 - using NOT
+select * from products
+where NOT 11.99;
+ 
+-- find  all products and sort them by price from greatest to least
+select * from products
+order by price desc;
+ 
+-- find all employees who don't have a middle initial
+select * from employees
+where middle_initial is NULL; 
+ 
+-- find distinct product prices
+select distinct products_prices from products;
+
+ 
+-- find all employees whose first name starts with the letter ‘j’
+select * from employees
+where firstname LIKE 'j%';
+ 
+-- find all Macbooks --
+select * from prodcuts
+where product = Macbooks;
+ 
+-- find all products that are on sale
+select * from products
+where On_Sale ; 
+ 
+-- find the average price of all products --
+select avg(price) from products;
+ 
+-- find all Geek Squad employees who don't have a middle initial --
+select * from employees
+where middle_initial is null
+order by Geek_Squad;
+
+ 
+-- find all products from the products table whose stock level is in the range of 500 to 1200. 
+-- Order by Price from least to greatest. **Try this with the between keyword
+select * from products 
+where stock_level between 500 and 1200
+order by price asc;
+
+
+
+
+
