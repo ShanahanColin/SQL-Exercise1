20ce50517a52578dda1562b788ba1ab0e748ca4d
Colin Shanny <cshannyy@gmail.com>
2020-11-28 16:49:31 -0600

    modified querys

--- SQLExercise1.sql
+++ SQLExercise1.sql
@@ -1,56 +1,55 @@
 -- find all products--
-select * from products;
+select * from products as p;
  
 -- find all products that cost $1400
 select * from products
-where price = 1400;
+where p.price = 1400;
  
 -- find all products that cost $11.99 or $13.99
 select * from products
-where price = 11.99 or 13.99;
+where p.Price = 11.99 or 13.99;
  
 -- find all products that do NOT cost 11.99 - using NOT
 select * from products
-where NOT 11.99;
+where NOT p.price = 11.99;
  
 -- find  all products and sort them by price from greatest to least
 select * from products
-order by price desc;
+order by p.price desc;
  
 -- find all employees who don't have a middle initial
-select * from employees
-where middle_initial is NULL; 
+select * from employees as e
+where e.middle_initial is NULL; 
  
 -- find distinct product prices
-select distinct products_prices from products;
+select distinct p.price from products;
 
  
 -- find all employees whose first name starts with the letter ‘j’
 select * from employees
-where firstname LIKE 'j%';
+where e.firstname LIKE 'j%';
  
 -- find all Macbooks --
 select * from prodcuts
-where product = Macbooks;
+where p.product = Macbooks;
  
 -- find all products that are on sale
 select * from products
-where On_Sale ; 
+WHERE p.Products = OnSale;
  
 -- find the average price of all products --
-select avg(price) from products;
+select avg(p.price) from products;
  
 -- find all Geek Squad employees who don't have a middle initial --
 select * from employees
-where middle_initial is null
-order by Geek_Squad;
+where e.MiddleInitial is null AND title= 'Geek Squad' ;
 
  
 -- find all products from the products table whose stock level is in the range of 500 to 1200. 
 -- Order by Price from least to greatest. **Try this with the between keyword
 select * from products 
-where stock_level between 500 and 1200
-order by price asc;
+where p.stocklevel between 500 and 1200
+order by p.price asc;
 
 
 
